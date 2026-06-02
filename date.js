1940ce3c5e0dd96c0e32ab2f40495dae6200dcd6
Pranav Mahajan <parumahajan3000@gmail.com>
2026-06-02 15:23:08 +0530

    Pushing code written for Date

---
+++ date.js
@@ -0,0 +1,21 @@
+let myDate = new Date()
+
+console.log(myDate)
+console.log("________________________________________________________________") 
+
+console.log(myDate.toString()) 
+console.log("________________________________________________________________") 
+
+console.log(myDate.toDateString())
+console.log("________________________________________________________________") 
+
+console.log(myDate.toISOString())
+console.log("________________________________________________________________") 
+
+console.log(myDate.toJSON())
+console.log("________________________________________________________________") 
+
+console.log(myDate.toLocaleDateString())
+console.log("________________________________________________________________") 
+
+console.log(myDate.toLocaleString())
