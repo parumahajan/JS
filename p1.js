28959befecf555522ad8fdef31295617b9c74383
Pranav Mahajan <parumahajan3000@gmail.com>
2026-06-02 19:52:38 +0530

    Pushed 3 pratice code files

---
+++ p1.js
@@ -0,0 +1,23 @@
+const product = {
+    name : "Parker Jotter Standard CT Ball Pen (Black)",
+    colour : "Black",
+    rating : 7002,
+    stars :4,
+    offer : "Deal of the Day",
+    discount : "5% off",
+    originalPrice : 285,
+    discountedPrice : 270,  
+};
+
+console.log(product);
+
+const profile = {
+    userName : "shradhakhapra" ,
+    isFollow: false,
+    posts : 195,
+    followers : 569000,
+    following : 4,
+    about : "Apna College",
+};
+
+console.log(typeof profile["userName"]);
---
+++ p3.js
@@ -0,0 +1,43 @@
+//const marks = {Maths:72, Science:80, English:85};
+
+// console.log(marks["Maths"]);
+//marks["Science"] = 1;
+//marks["Science"] += 1;
+// console.log(marks.Science);
+
+//_________________________________________________________
+// const profile = {
+//     userName : "shradhakhapra" ,
+//     isFollow: false,
+//     posts : 195,
+//     followers : 569000,
+//     following : 4,
+//     about : "Apna College",
+// };
+
+//console.log(typeof profile["userName"]);
+
+//_________________________________________________________
+// let number = prompt("Enter a number: ");
+
+// if(number % 5 == 0){
+//     console.log(number, "is multiple of 5");
+// }
+// else{
+//     console.log(number, "is not a multile of 5");
+// }
+
+
+// const profile = {
+//     userName : "shradhakhapra" ,
+//     isFollow: false,
+//     posts : 195,
+//     followers : 569000,
+//     following : 4,
+//     about : "Apna College",
+// };
+
+// for(let i in profile){
+//     console.log(i);
+// }
+
