8c80cfdc5d09baba250c9ba9a3c80c50075eb179
Pranav Mahajan <parumahajan3000@gmail.com>
2026-06-02 10:58:21 +0530

    Practiced some codes on Date

--- 1trial.js
+++ 1trial.js
@@ -1,1 +1,21 @@
-console.log((Math.random() * 10) + 1) 
+let myDate = new Date()
+
+console.log(myDate)
+console.log("________________________________________________________________") 
+
+console.log(myDate.toString()) 
+console.log("________________________________________________________________") 
+
+console.log(myDate.toDateString())
+console.log("________________________________________________________________") 
+
+console.log(myDate.toISOString())
+console.log("________________________________________________________________") 
+
+console.log(myDate.toJSON())
+console.log("________________________________________________________________") 
+
+console.log(myDate.toLocaleDateString())
+console.log("________________________________________________________________") 
+
+console.log(myDate.toLocaleString())
