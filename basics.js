dcb074d19a0d5df67109c81274be2f38bc22e9a2
Pranav Mahajan <parumahajan3000@gmail.com>
2026-06-05 14:07:47 +0530

    Completed notes till Arrow Function

--- basics.js
+++ basics.js
@@ -1097,6 +1097,51 @@ console.log(this);               // --> {}
 
 When we print this outside the function, then we get only "undefined", but when we print this inside a function, then we see, we get a lot of values.
 _______________________________________________________________________________________________________________
+* ARROW FUNCTIONS
+
+Arrow functions do not have their own this. We can use this, but they inherit this from the surrounding scope.
+
+& Usually how we define fn using variable is like this:
+const user = function(){
+    let username = "Pranav"
+    console.log(this.username)     // --> undefined 
+    console.log(this)              // --> many values
+}
+user()
+
+& Now, if we want to use Arrow functions here, then we have to do this:
+1) Remove 'function' keyword.
+2) Write "=>" infront of "()".
+
+const user = () => {
+    let username = "Pranav"
+    console.log(this.username)   // --> undefined 
+    console.log(this)            // --> {}
+}
+user()
+
+& Syntax:  <DT> <Var> = () => {}
+
+    const add_two_nums = (num1, num2) => {
+    return num1 + num2
+}
+
+console.log(add_two_nums(5, 4)); 
+
+* IMPLICIT RETURN
+
+Here, we remove the Curly Brackets { } and the 'return' keyword.
+We bring the rest content on the same line.
+
+const add_nums = (num1, num2) => (num1 + num2)
+console.log(add_nums);
+
+If we use {} => write 'return' keyword (EXPLICIT)
+Only  use () => no need to write 'return' keyword (IMPLICIT)
+
+const add_nums = (num1, num2) => ({username: "Pranav"})
+console.log(add_nums);
+_______________________________________________________________________________________________________________
 * HOISTING
 -> Declarations are moved to the top. (Not initializations)
 
