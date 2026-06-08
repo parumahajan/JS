7abfb3ae89f9e9b59d79b586bd35a687b2b00021
Pranav Mahajan <parumahajan3000@gmail.com>
2026-06-09 02:18:00 +0530

    Made notes till Control Flow

--- basics.js
+++ basics.js
@@ -623,6 +623,39 @@ switch(fruit){
     default:
         console.log('Sorry, we are out of ${fruit}');
 }
+
+for(let i = 1; i <= 7; i++){
+    if(i == 5){
+        console.log(`5 is Detected`)
+        break
+        //continue
+    }
+    console.log(`Value of i (with break) is ${i}`)
+}
+_______________________________________________________________________________________________________________
+* CONTINUE 
+Value of i (without continue) is 1
+Value of i (without continue) is 2
+Value of i (without continue) is 3
+Value of i (without continue) is 4
+5 is Detected
+Value of i (without continue) is 5
+Value of i (without continue) is 6
+Value of i (without continue) is 7
+
+Value of i (with continue) is 1
+Value of i (with continue) is 2
+Value of i (with continue) is 3
+Value of i (with continue) is 4
+5 is Detected
+Value of i (with continue) is 6
+Value of i (with continue) is 7
+
+Value of i (with break) is 1
+Value of i (with break) is 2
+Value of i (with break) is 3
+Value of i (with break) is 4
+5 is Detected
 _______________________________________________________________________________________________________________
 * LOOPS
 
