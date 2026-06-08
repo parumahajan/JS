e7f7f32c7a995877a820566bf821c4430cac2354
Pranav Mahajan <parumahajan3000@gmail.com>
2026-06-09 02:19:27 +0530

    Made notes about Map, and practiced it.

--- basics.js
+++ basics.js
@@ -729,12 +729,56 @@ const profile = {
 for(let i in student){
     console.log(i);
 }
-
-// For-in loop doesn't work on Arrays. We can use it on Objects, but not Arrays.
-// For-of loop can be applied on both arrays and objects.
+/*
+For-in loop doesn't work on Arrays. We can use it on Objects, but not Arrays.
+For-of loop can be applied on both arrays and objects.
 _______________________________________________________________________________________________________________
 * MAPS
+ 
+We don't get Duplicate values. We only get Unique values.
+Map is itself is an object.
+We set a key-value pair.
+*/
 
+const map = new Map()   // we created its object first 
+
+map.set('IN', "India")
+map.set('US', "United States")
+map.set('FR', "France")
+map.set('IN', "India")
+
+console.log(map);
+
+/* OUTPUT
+Map(3) {
+  'IN' => 'India',
+  'US' => 'United States',
+  'FR' => 'France'
+*/
+
+// Now, we try giving Iterator as "Key" and "Value"
+for(const [key, value] of map){
+    console.log(key, ' :- ' , value );
+}
+
+/* OUTPUT 
+IN :-  India
+US :-  United States
+FR :-  France
+*/
+// _____________________________________________________________________________________________________________
+
+// Now, we will try operating FOR-OF loop on "Strings"
+const myObject = {
+    'game1': 'NFS',
+    'game2': 'IGI'
+}
+
+for(const [key, value] of myObject){
+    console.log((key, ":-", value));
+}
+
+// It will throw an error: myObject is not iterable. 
 _______________________________________________________________________________________________________________
 * CONTROL FLOW / LOGIC FLOW 
 Always entire code should not be ran.
